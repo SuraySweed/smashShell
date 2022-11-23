ad96f29acfbcc49dd9645b7e666d2985efe604f1
student <suray263@gmail.co>
2022-11-23 19:49:17 +0000

    smash cpp file

--- smash.cpp
+++ smash.cpp
@@ -6,18 +6,24 @@
 #include "signals.h"
 
 int main(int argc, char* argv[]) {
-    if(signal(SIGTSTP , ctrlZHandler)==SIG_ERR) {
+    if(signal(SIGTSTP, ctrlZHandler) == SIG_ERR) {
         perror("smash error: failed to set ctrl-Z handler");
     }
-    if(signal(SIGINT , ctrlCHandler)==SIG_ERR) {
+    if(signal(SIGINT, ctrlCHandler) == SIG_ERR) {
         perror("smash error: failed to set ctrl-C handler");
     }
 
-    //TODO: setup sig alarm handler
+    struct sigaction sigaction_alarm;
+    sigaction_alarm.sa_handler = &alarmHandler;
+    sigaction_alarm.sa_flags = SA_RESTART;
+
+    if(sigaction(SIGALRM,&sigaction_alarm, nullptr) < 0){
+        perror("smash error: failed to set SIGALARM handler");
+    }
 
     SmallShell& smash = SmallShell::getInstance();
     while(true) {
-        std::cout << "smash> ";
+        std::cout << smash.getCurrentPrompt() << "> ";
         std::string cmd_line;
         std::getline(std::cin, cmd_line);
         smash.executeCommand(cmd_line.c_str());
